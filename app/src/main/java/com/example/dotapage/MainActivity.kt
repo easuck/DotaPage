f903b64c75046590e3a528f879c8365a2e35b6ed
Егор Асонов <egorasonovvv@gmail.com>
2023-11-07 12:00:33 +0600

    [+] Implemented elements from step 2

--- app/src/main/java/com/example/dotapage/MainActivity.kt
+++ app/src/main/java/com/example/dotapage/MainActivity.kt
@@ -8,20 +8,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,13 +41,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.WindowCompat
 import com.example.dotapage.ui.theme.DotaPageTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -96,7 +98,8 @@ fun DotaScreen(){
                     end = 24.dp,
                     top = 69.dp,
                     bottom = 43.dp
-                )
+                ),
+                fontSize = 12.sp
             )
         }
         item{
@@ -107,9 +110,81 @@ fun DotaScreen(){
                 )
             )
         }
+        item{
+            Text(
+                text = stringResource(id = R.string.comments),
+                color = Color.White,
+                modifier = Modifier.padding(
+                    top = 20.dp,
+                    bottom = 29.dp,
+                    start = 24.dp
+                ),
+                fontSize = 16.sp
+            )
+        }
+        item{
+            Comments()
+        }
         item {
             CustomButton()
         }
+    }
+}
+
+@Preview
+@Composable
+fun Comments(){
+    Column {
+        CommentBlock(R.drawable.user_1, R.string.username_1, R.string.date_1, R.string.comment_1)
+        Divider(
+            color = Color.Gray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(
+                start = 37.dp,
+                end = 37.dp,
+                top = 24.dp,
+                bottom = 24.dp
+            )
+        )
+        CommentBlock(R.drawable.user_2, R.string.username_2, R.string.date_2, R.string.comment_2)
+    }
+}
+
+
+@Composable
+fun CommentBlock(userImage:Int, username: Int, date: Int, comment: Int){
+    Column(modifier = Modifier.padding(
+        start = 24.dp,
+        end = 24.dp
+    )) {
+        Row(){
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(36.dp)
+            ){
+                Image(
+                    painter = painterResource(id = userImage),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = stringResource(id = username),
+                    color = Color.White,
+                    fontSize = 16.sp)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = stringResource(id = date),
+                    color = Color.White,
+                    fontSize = 12.sp)
+            }
+        }
+        Spacer(modifier = Modifier.height(17.dp))
+        Text(text = stringResource(id = comment),
+            color = Color.White,
+            fontSize = 12.sp)
     }
 }
 
